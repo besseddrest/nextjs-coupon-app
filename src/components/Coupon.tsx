6d6db769e5498f39e666d0a8c1aa80694d0d6181
Harold C <hcabalic@gmail.com>
2023-06-16 17:09:41 -0700

    Init setup

---
+++ src/components/Coupon.tsx
@@ -0,0 +1,12 @@
+export default function Coupon() {
+  return (
+    <div className="coupon">
+      <h3>Foo Bar</h3>
+      <p>
+        This is the coupon description
+      </p>
+      <div className="coupon__value">69 pts</div>
+      <button>Redeem</button>
+    </div>
+  )
+}
