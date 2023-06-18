91627c429da20a0bd939bfd62f724a5e2b395661
Harold C <hcabalic@gmail.com>
2023-06-17 21:39:11 -0700

    Add tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,18 @@ export default function Home() {
   const [balance, setBalance] = useState<number>(AVAILABLE_BALANCE)
   const [totalRedeemed, setTotalRedeemed] = useState<number | null>(null)
 
+  useEffect(() => {
+    let pointsRedeemed = 0
+
+    allCoupons.forEach((item: Coupon) => {
+      if (item.isRedeemed) {
+        pointsRedeemed = pointsRedeemed + item.value
+      }
+    })
+
+    setTotalRedeemed(pointsRedeemed)
+  }, [allCoupons])
+
   function handleRedeem(index: number) {
     const tempCoupons = [...allCoupons]
     let pointsRedeemed = tempCoupons[index].value;
@@ -26,28 +38,16 @@ export default function Home() {
     setTotalRedeemed(totalRedeemed ? totalRedeemed + points : points);    
   }
 
-  useEffect(() => {
-    let pointsRedeemed = 0
-
-    allCoupons.forEach((item: Coupon) => {
-      if (item.isRedeemed) {
-        pointsRedeemed = pointsRedeemed + item.value
-      }
-    })
-
-    setTotalRedeemed(pointsRedeemed)
-  }, [allCoupons])
-
   return (
-    <main className="content">
-      <h1>Coupon App</h1>
-      <div className="total-points-available flex-auto">
-        You have earned a total of { balance } points!
+    <main className="content h-full p-4">
+      <h1 className="font-bold text-4xl mb-2">Coupon App</h1>
+      <div className="total-points-available flex-auto mb-4">
+        You have earned a total of <span className="font-bold text-xl">{ balance }</span> points!
       </div>
       <div className="coupons-container flex">
-        <div className="available-coupons flex-auto">
+        <div className="available-coupons flex-auto w-2/4 pr-4">
           {/* List of available coupons */}
-          <h2>Available</h2>
+          <h2 className="font-bold text-2xl mb-4">Available</h2>
           <div className="coupons">
             {
               allCoupons.map((item: Coupon, i: number) => {
@@ -56,9 +56,9 @@ export default function Home() {
             }
           </div>
         </div>
-        <div className="redeemed-coupons flex-auto">
+        <div className="redeemed-coupons flex-auto w-2/4 pl-4">
           {/* List of redeemed coupons */}
-          <h2>Redeemed</h2>
+          <h2 className="font-bold text-2xl mb-4">Redeemed</h2>
           <div className="coupons">
             {
               allCoupons.map((item: Coupon, i: number) => {
@@ -67,7 +67,7 @@ export default function Home() {
             }
           </div>
           <div className="total-points-redeemed">
-            Total points spent: { totalRedeemed }
+            Total points spent: <span className="font-bold text-lg text-red-700">{ totalRedeemed }</span>
           </div>
         </div>
       </div>
--- src/components/Coupon.tsx
+++ src/components/Coupon.tsx
@@ -1,17 +1,21 @@
 import Coupon from "@/interfaces/coupon"
 
 export default function CouponComponent(props: { item: Coupon, handleRedeem(index: number): void, index: number }) {
-  const { id, name, description, value } = props.item
+  const { id, name, description, value, isRedeemed } = props.item
   const handleRedeem = props.handleRedeem;
   const index = props.index
   return (
-    <div id={`item-${ id }`} className="coupon">
-      <h3>{ name }</h3>
-      <p>
+    <div id={`item-${ id }`} className="coupon mb-4 px-3 py-2 rounded border border-black bg-white">
+      <h3 className="font-bold text-lg">{ name }</h3>
+      <p className="mb-2">
         { description }
       </p>
-      <div className="coupon__value">{ value } pts</div>
-      <button onClick={() => handleRedeem(index) }>Redeem</button>
+      <div className="coupon__value mb-2"><span className="text-green-600 font-bold text-lg">{ value }</span> pts</div>
+      {
+        !isRedeemed
+          ? <button className="mb-2 text-white bg-blue-600 border-white rounded px-3 py-2" onClick={() => handleRedeem(index) }>Redeem</button>
+          : <button className="mb-2 text-white bg-gray-600 border-white rounded px-3 py-2" onClick={() => handleRedeem(index) }>Undo Redeem</button>
+      }
     </div>
   )
 }
